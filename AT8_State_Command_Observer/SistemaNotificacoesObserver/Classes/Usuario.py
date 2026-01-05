11110f8950b859c9f0dfd1e748b7fd45fde31304
damnd <eduardasaamanta@gmail.com>
2026-01-04 21:46:22 -0300

    # corrigindo classe Usuário AT8

--- AT8_State_Command_Observer/SistemaNotificacoesObserver/Classes/Usuario.py
+++ AT8_State_Command_Observer/SistemaNotificacoesObserver/Classes/Usuario.py
@@ -1,7 +1,7 @@
-from abc import ABC
+from AT8_State_Command_Observer.SistemaNotificacoesObserver.Interface.Observador import Observador
 
 
-class Usuario(ABC):
+class Usuario(Observador):
     def __init__(self, nome: str):
         self.nome = nome
 
